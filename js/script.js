e2abd55c152c36557ee0cf5e3e6019ff90afdd1f
ILTON JUNIO CAETANO DE SOUZA <129558260+iltonjunio@users.noreply.github.com>
2023-06-24 08:40:18 -0300

    d

--- js/script.js
+++ js/script.js
@@ -1,76 +1,5 @@
-// class Cliente {
-//     constructor(cpf, nome, datanascimento) {
-//     this.cpf = cpf;
-//     this.nome = nome;
-//     this.datanascimento = datanascimento;
-//     }
-// }
 
-// class Produto {
-//     constructor(codigo, descricao, qtdestoque) {
-//     this.codigo = codigo;
-//     this.descricao = descricao;
-//     this.qtdestoque = qtdestoque;
-//     }
-// }
-
-// class Venda {
-//     constructor(codigo, datahora, cliente, produto) {
-//     this.codigo = codigo;
-//     this.datahora = datahora;
-//     this.cliente = cliente;
-//     this.produto = produto;
-//     }
-// }
-
-//   // Exercício 02
-//   // Instanciando objetos para a classe Cliente
-// const clientes = [
-//     new Cliente('111.111.111-11', 'João', '1990-01-01'),
-//     new Cliente('222.222.222-22', 'Maria', '1985-05-10'),
-//     new Cliente('333.333.333-33', 'Pedro', '1998-12-15')
-// ];
-
-//   // Instanciando objetos para a classe Produto
-// const produtos = [
-//     new Produto('001', 'Camiseta', 10),
-//     new Produto('002', 'Calça Jeans', 5),
-//     new Produto('003', 'Tênis', 15),
-//     new Produto('004', 'Boné', 20),
-//     new Produto('005', 'Óculos de Sol', 8),
-//     new Produto('006', 'Bolsa', 12),
-//     new Produto('007', 'Chapéu', 30),
-//     new Produto('008', 'Jaqueta', 3)
-// ];
-
-//   // Instanciando objetos para a classe Venda
-// const vendas = [
-//     new Venda('001', '2023-06-23 10:00:00', clientes[0], produtos[0]),
-//     new Venda('002', '2023-06-23 11:30:00', clientes[1], produtos[2]),
-//     new Venda('003', '2023-06-23 14:45:00', clientes[2], produtos[4]),
-//     new Venda('004', '2023-06-24 09:15:00', clientes[0], produtos[6]),
-//     new Venda('005', '2023-06-24 16:30:00', clientes[1], produtos[1])
-// ];
-
-//   // Exercício 03
-//   // Exibindo os nomes dos clientes
-// console.log("Nomes dos clientes:");
-// clientes.forEach(cliente => console.log(cliente.nome));
-
-//   // Exibindo as descrições dos produtos
-// console.log("\nDescrições dos produtos:");
-// produtos.forEach(produto => console.log(produto.descricao));
-
-//   // Exibindo as datahora das vendas em formato JavaScript
-// console.log("\nDatahora das vendas (formato JavaScript):");
-// vendas.forEach(venda => console.log(venda.datahora));
-
-//   // Exercício 5
-//   // Transformando o array de clientes em um objeto Map
-// const clientesMap = new Map(
-//     clientes.map(cliente => [cliente])
-// )
-// Exercício 1
+//exercicio 1
 class Cliente {
   constructor(cpf, nome, datanascimento) {
     this.cpf = cpf;
@@ -123,22 +52,93 @@ const vendas = [
 ];
 
 // Exercício 3
-const clienteNomes = clientes.map(cliente => cliente.nome);
-const produtoDescricoes = produtos.map(produto => produto.descricao);
-const vendaDataHoras = vendas.map(venda => venda.datahora);
+// Array de clientes
+const cliente = [
+  { cpf: '11111111111', nome: 'João' },
+  { cpf: '22222222222', nome: 'Maria' },
+  { cpf: '33333333333', nome: 'Pedro' }
+];
 
-console.log('Nomes dos clientes:', clienteNomes);
-console.log('Descrições dos produtos:', produtoDescricoes);
-console.log('Datas e horas das vendas:', vendaDataHoras);
+// Array de produtos
+const produto = [
+  { codigo: 'P1', descricao: 'Camiseta', qtdestoque: 10 },
+  { codigo: 'P2', descricao: 'Calça', qtdestoque: 5 },
+  { codigo: 'P3', descricao: 'Boné', qtdestoque: 20 }
+];
+
+// Array de vendas
+const venda = [
+  { codigo: 'V1', datahora: '2023-06-01 10:30', cliente: '11111111111', produto: 'P1' },
+  { codigo: 'V2', datahora: '2023-06-02 15:45', cliente: '22222222222', produto: 'P2' },
+  { codigo: 'V3', datahora: '2023-06-03 09:15', cliente: '33333333333', produto: 'P3' }
+];
+
+// Exibindo os atributos de cada classe
+console.log('\n\nAtributos dos clientes:');
+for (let i = 0; i < clientes.length; i++) {
+  const cliente = clientes[i];
+  console.log('Nome:', cliente.nome);
+}
+
+console.log('\n\nAtributos dos produtos:');
+for (let i = 0; i < produtos.length; i++) {
+  const produto = produtos[i];
+  console.log('Descrição:', produto.descricao);
+}
+
+console.log('\n\nAtributos das vendas:');
+for (let i = 0; i < vendas.length; i++) {
+  const venda = vendas[i];
+  console.log('Data/Hora:', venda.datahora);
+}
+
 
 // Exercício 4
-const clienteMap = new Map(clientes.map(cliente => [cliente.cpf, [cliente.nome, cliente.datanascimento]]));
-const produtoMap = new Map(produtos.map(produto => [produto.codigo, [produto.descricao, produto.qtdestoque]]));
-const vendaMap = new Map(vendas.map(venda => [venda.codigo, [venda.datahora, venda.cliente, venda.produto]]));
 
-console.log('Mapa de clientes:', clienteMap);
-console.log('Mapa de produtos:', produtoMap);
-console.log('Mapa de vendas:', vendaMap);
+const clienteMap = new Map();
+for (let i = 0; i < clientes.length; i++) {
+  const cliente = clientes[i];
+  clienteMap.set(cliente.cpf, { nome: cliente.nome, datanascimento: cliente.datanascimento });
+}
+
+const produtoMap = new Map();
+for (let i = 0; i < produtos.length; i++) {
+  const produto = produtos[i];
+  produtoMap.set(produto.codigo, { descricao: produto.descricao, qtdestoque: produto.qtdestoque });
+}
+
+const vendaMap = new Map();
+for (let i = 0; i < vendas.length; i++) {
+  const venda = vendas[i];
+  vendaMap.set(venda.codigo, { datahora: venda.datahora, cliente: venda.cliente, produto: venda.produto });
+}
+
+console.log('\n\nMapa de clientes (objetos):', clienteMap);
+console.log('\n\nMapa de produtos (objetos):', produtoMap);
+console.log('\n\nMapa de vendas (objetos):', vendaMap);
+
+
 
 // Exercício 5
-const clienteObj = new Map(clientes.map(cliente => [cliente.cpf, { nome: cliente.nome, datanascimento: cliente.datanascimento }]))
+// Transformando os arrays em objetos Map
+const clienteMap1 = new Map();
+for (let i = 0; i < clientes.length; i++) {
+  const cliente = clientes[i];
+  clienteMap.set(cliente.cpf, { nome: cliente.nome, datanascimento: cliente.datanascimento });
+}
+
+const produtoMap1 = new Map();
+for (let i = 0; i < produtos.length; i++) {
+  const produto = produtos[i];
+  produtoMap.set(produto.codigo, { descricao: produto.descricao, qtdestoque: produto.qtdestoque });
+}
+
+const vendaMap1 = new Map();
+for (let i = 0; i < vendas.length; i++) {
+  const venda = vendas[i];
+  vendaMap.set(venda.codigo, { datahora: venda.datahora, cliente: venda.cliente, produto: venda.produto });
+}
+
+console.log('\nMapa de clientes (objetos):', clienteMap);
+console.log('\nMapa de produtos (objetos):', produtoMap);
+console.log('\nMapa de vendas (objetos):', vendaMap);
